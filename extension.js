c3be5ee87ffd5d9d08a3c5b03a3f5ffb4bb68a53
Eggy-s <78775522+Eggy-s@users.noreply.github.com>
2024-08-27 00:46:17 -0700

    Update extension.js
    Added useless explanation

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 
 
 
-let color_on_select = `#e66159b3`;
+let color_on_select = `#e66159b3`; /* In case you're curious why I have a useless variable for the color its because I'm lazy and originally wanted the user to be able to change it*/
 
 
 
